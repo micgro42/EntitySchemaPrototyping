e88c111f94f48a22f4af3445ef22072ab73ba418
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-10-27 15:13:07 +0200

    Add handler for SearchEntities hook
    Change-Id: I9f57b6d92c39c3ae596e38773f6e6f479ff4b7dc
    See-Also: I99e1a08c1ed64703528a8da4abaa102948c498a7

--- src/Wikibase/Hooks/WikibasePseudoEntitiesHandler.php
+++ src/Wikibase/Hooks/WikibasePseudoEntitiesHandler.php
@@ -4,7 +4,9 @@ declare( strict_types = 1 );
 namespace EntitySchema\Wikibase\Hooks;
 
 use EntitySchema\Domain\Model\EntitySchemaId;
+use InvalidArgumentException;
 use LinkBatch;
+use MediaWiki\Title\Title;
 use Wikibase\DataModel\Entity\EntityId;
 
 /**
@@ -23,6 +25,28 @@ class WikibasePseudoEntitiesHandler {
 			return false;
 		}
 		return true;
+	}
+
+	public function onWikibasePseudoEntities_SearchEntities_Handlers(
+		array &$pseudoEntityTypeHandlers
+	): void {
+		$pseudoEntityTypeHandlers['entityschema'] = static function ( array $params ): array {
+			try {
+				$id = new EntitySchemaId( $params['search'] );
+			} catch ( InvalidArgumentException $exception ) {
+				return [];
+			}
+			$title = Title::makeTitleSafe( NS_ENTITYSCHEMA_JSON, $id ); // TODO use TitleFactory
+			if ( $title->exists() ) {
+				return [ [
+					'id' => $id->getSerialization(),
+					'title' => $title->getPrefixedText(),
+					'pageid' => $title->getArticleID(),
+					'display' => [], // TODO use EntitySchemaConverter::getMonolingualNameBadgeData()?
+				] ];
+			}
+			return [];
+		};
 	}
 
 	// phpcs:enable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
