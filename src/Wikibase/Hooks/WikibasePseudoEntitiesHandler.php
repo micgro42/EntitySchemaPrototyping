6dd214acb18db8a8360115c678e72d6af396648e
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-10-27 15:13:07 +0200

    Add handler for SearchEntities hook
    Change-Id: I9f57b6d92c39c3ae596e38773f6e6f479ff4b7dc
    See-Also: I99e1a08c1ed64703528a8da4abaa102948c498a7

--- src/Wikibase/Hooks/WikibasePseudoEntitiesHandler.php
+++ src/Wikibase/Hooks/WikibasePseudoEntitiesHandler.php
@@ -5,6 +5,8 @@ declare( strict_types = 1 );
 namespace EntitySchema\Wikibase\Hooks;
 
 use EntitySchema\Domain\Model\EntitySchemaId;
+use InvalidArgumentException;
+use MediaWiki\Title\Title;
 
 /**
  * @license GPL-2.0-or-later
@@ -17,4 +19,30 @@ class WikibasePseudoEntitiesHandler {
 			return new EntitySchemaId( $idSerialization );
 		};
 	}
+
+	// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+	public function onWikibasePseudoEntities_SearchEntities_Handlers(
+		array &$pseudoEntityTypeHandlers
+	): void {
+		$pseudoEntityTypeHandlers['entityschema'] = static function ( array $params ): array {
+			try {
+				$id = new EntitySchemaId( $params['search'] );
+			} catch ( InvalidArgumentException $exception ) {
+				return [];
+			}
+			$title = Title::makeTitleSafe( NS_ENTITYSCHEMA_JSON, $id ); // TODO use TitleFactory
+			if ( $title->exists() ) {
+				return [ [
+					'id' => $id->getSerialization(),
+					'title' => $title->getPrefixedText(),
+					'pageid' => $title->getArticleID(),
+					'display' => [], // TODO use EntitySchemaConverter::getMonolingualNameBadgeData()?
+				] ];
+			}
+			return [];
+		};
+	}
+
+	// phpcs:enable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
 }
