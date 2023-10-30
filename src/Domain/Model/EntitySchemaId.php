3e81acf299ee836ccded47ae886eaeee4e2d9a1e
Michael Große <michael.grosse@wikimedia.de>
2023-10-30 18:44:50 +0100

    Make EntitySchemaId implement PseudoEntityId
    Depends-On: I52e7ef30b684d7828c3a57c34569db7f2d5dde73
    Change-Id: I05f43a2f67d754430d80628b91acc7c160262916

--- src/Domain/Model/EntitySchemaId.php
+++ src/Domain/Model/EntitySchemaId.php
@@ -5,11 +5,13 @@ declare( strict_types = 1 );
 namespace EntitySchema\Domain\Model;
 
 use InvalidArgumentException;
+use Wikibase\DataModel\Entity\Int32EntityId;
+use Wikibase\DataModel\Entity\PseudoEntityId;
 
 /**
  * @license GPL-2.0-or-later
  */
-class EntitySchemaId {
+class EntitySchemaId implements PseudoEntityId, Int32EntityId {
 
 	private string $id;
 
@@ -20,6 +22,10 @@ class EntitySchemaId {
 			throw new InvalidArgumentException( 'ID must match ' . self::PATTERN );
 		}
 
+		if ( strlen( $id ) > 10 && substr( $id, 1 ) > Int32EntityId::MAX ) {
+			throw new InvalidArgumentException( 'ID can not exceed ' . Int32EntityId::MAX );
+		}
+
 		$this->id = $id;
 	}
 
@@ -27,4 +33,39 @@ class EntitySchemaId {
 		return $this->id;
 	}
 
+	public function getEntityType() {
+		return 'entityschema';
+	}
+
+	public function getSerialization() {
+		return $this->id;
+	}
+
+	public function __toString() {
+		return $this->id;
+	}
+
+	public function equals( $target ) {
+		if ( $this === $target ) {
+			return true;
+		}
+
+		return $target instanceof self
+			&& $target->id === $this->id;
+	}
+
+	public function __serialize(): array {
+		return [ 'serialization' => $this->id ];
+	}
+
+	public function __unserialize( array $data ) {
+		$this->__construct( $data['serialization'] );
+		if ( $this->id !== $data['serialization'] ) {
+			throw new InvalidArgumentException( '$data contained invalid serialization' );
+		}
+	}
+
+	public function getNumericId() {
+		return (int)substr( $this->id, 1 );
+	}
 }
